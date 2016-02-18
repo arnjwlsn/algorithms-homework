99032657493f9178c5697d82ed600d11ae842c14
Aaron Wilson <awilson2994@gmail.com>
2016-02-17 20:18:41 -0500

    Final commit

--- search.cpp
+++ search.cpp
@@ -5,12 +5,14 @@
 #include <vector>
 #include <string>
 
+const int FINAL_ARRAY_SIZE = 400000;
 const int MAX_RAND_VALUE = 100000;
 const int INCREMENT_SIZE = 5000;
-const int TRIALS_PER_TEST = 10;
+const int INITIAL_SIZE = 5000;
+const int TRIALS_PER_TEST = 100;
 const std::string TIME_UNIT = "1*10^-6s";
 const int DIVIDE_TIME = 1000000;
-int _size = 5000;
+int _size = INITIAL_SIZE;
 
 // Fill the array with random values that range from 0 - 100,000
 void fill_array(int *list) {
@@ -19,18 +21,30 @@ void fill_array(int *list) {
    }
 }
 
-// Sort the array in ascending order
-void sort_array(int *list) {
-   for(int i = 0; i < _size - 1; ++i) {
-      for(int j = i; j < _size; ++j) {
-         if(list[i] > list[j]) {
-            int tmp = 0;
-            tmp = list[i];
-            list[i] = list[j];
-            list[j] = tmp;
-         }
+// Quick sort to sort array in ascending order
+void quick_sort(int *list, int left, int right) {
+   int i = left, j = right;
+   int pivot = list[(left + right)/2];
+   int tmp;
+
+   while(i <= j) {
+      while(list[i] < pivot)
+         i++;
+      while(list[j] > pivot)
+         j--;
+      if(i <= j) {
+         tmp = list[i];
+         list[i] = list[j];
+         list[j] = tmp;
+         i++;
+         j--;
       }
    }
+
+   if(left < j)
+      quick_sort(list, left, j);
+   if(i < right)
+      quick_sort(list, i, right);
 }
 
 int binary_search(int *list, int key) {
@@ -63,13 +77,6 @@ int ternary_search(int *list, int key) {
    }
 }
 
-int linear_search(int *list, int key) {
-   for(int i = 0; i < _size; ++i) {
-      if(list[i] == key) return i;
-   }
-   return -1;
-}
-
 double average(std::vector<double> vec) {
    double average = 0;
    for(std::vector<double>::iterator it = vec.begin(); it != vec.end(); ++it) {
@@ -78,30 +85,26 @@ double average(std::vector<double> vec) {
    return average/(double)vec.size();
 }
 
-std::ostream& operator<<(std::ostream& out, const int *list) {
-   for(int i = 0; i < _size; ++i) {
-      out << list[i] << std::endl;   
-   }
-   return out;
-}
-
 double clock_time(clock_t start, clock_t end) {
    double per_micros = (CLOCKS_PER_SEC/DIVIDE_TIME);
    return ((double)end - (double)start) / per_micros;
 }
 
-void display(int key, int location, clock_t start, clock_t end) {
-   std::cout << "Key: " << key << std::endl;
-   std::cout << "Location: " << location << std::endl;
-   std::cout << "Time: " << clock_time(start, end) << " " << TIME_UNIT  << std::endl;
+std::ostream& operator << (std::ostream& out, const int *list) {
+   for(int i = 0; i < _size; ++i) {
+      out << list[i] << std::endl;
+   }
+   return out;
 }
 
 int main() {
    clock_t start, end;
    int key = 0, location = 0, counter = 0;
-   std::vector<double> tern, bin, lin;
+   std::vector<double> tern_avg, bin_avg;
+   std::vector<int> time_res;
+   std::vector<double> tern_res, bin_res;
    srand(time(NULL));
-   
+
    std::ofstream myfile;
    myfile.open("results.csv");
    myfile << "No. of Element in Array,"
@@ -109,12 +112,16 @@ int main() {
           << "Binary Search Time (" << TIME_UNIT << ")" 
           << std::endl;
 
-   while(_size <= 100000) {
+   // Ternary Search Test
+   std::cout << "-------------------\n" 
+             << "| Ternary Search: |\n" 
+             << "-------------------" << std::endl;
+   while(_size <= FINAL_ARRAY_SIZE) {
       int list[_size];
       
       fill_array(list);
-      sort_array(list);
-      myfile << _size << ",";
+      quick_sort(list, 0, _size - 1);
+      time_res.push_back(_size);
 
       std::cout << _size << "..." << std::endl;
    
@@ -125,31 +132,16 @@ int main() {
          start = clock();
          location = ternary_search(list, key);
          end = clock();
-         tern.push_back(clock_time(start, end));
-
-         // Binary Search
-         start = clock();
-         location = binary_search(list, key);
-         end = clock();
-         bin.push_back(clock_time(start, end));
-
-         /*/ Linear Search
-         start = clock();
-         location = linear_search(list, key);
-         end = clock();
-         lin.push_back(clock_time(start, end));*/
+         tern_avg.push_back(clock_time(start, end));
 
          //increment the counter
          counter++;
       }
-      
-      myfile << average(tern) << ","
-             << average(bin) <<  std::endl;
+
+      tern_res.push_back(average(tern_avg));
 
       //clear vectors
-      tern.clear();
-      bin.clear();
-      //lin.clear();
+      tern_avg.clear();
 
       //reset counter
       counter = 0;
@@ -157,6 +149,50 @@ int main() {
       //increment the array size
       _size += INCREMENT_SIZE;
    }   
+
+   _size = INITIAL_SIZE;
+
+   // Binary Search Test
+   std::cout << "------------------\n" 
+             << "| Binary Search: |\n" 
+             << "------------------" << std::endl;
+   while(_size <= FINAL_ARRAY_SIZE) {
+      int list[_size];
+      
+      fill_array(list);
+      quick_sort(list, 0, _size - 1);
+
+      std::cout << _size << "..." << std::endl;
+   
+      while(counter < TRIALS_PER_TEST) {
+         key = rand() % (MAX_RAND_VALUE + 1);
+
+         // Binary Search
+         start = clock();
+         location = binary_search(list, key);
+         end = clock();
+         bin_avg.push_back(clock_time(start, end));
+
+         //increment the counter
+         counter++;
+      }
+
+      bin_res.push_back(average(bin_avg));
+
+      //clear vectors
+      bin_avg.clear();
+
+      //reset counter
+      counter = 0;
+
+      //increment the array size
+      _size += INCREMENT_SIZE;
+   }
+
+   //write data values to file
+   for(int i = 0; i < time_res.size(); ++i) {
+      myfile << time_res[i] << "," << tern_res[i] << "," << bin_res[i] << std::endl;
+   }
 
    myfile.close();
    return 0;
