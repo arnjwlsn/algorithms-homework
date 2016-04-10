a66e641110dca1de64e44d4d5244d3ef0ac498db
awilson2994 <awilson2994@gmail.com>
2016-04-10 15:56:40 -0600

    Update convex_hull.cpp

--- hw3/convex_hull.cpp
+++ hw3/convex_hull.cpp
@@ -24,22 +24,22 @@ const std::string TIME_UNIT = "ms";
 const int DIVIDE_TIME = 1000;
 size_t number_of_points = INITIAL_SIZE;
 
-struct Point {
+struct Coordinate {
    float x;
    float y;
 
-   bool operator<(const Point &pt) const {
+   bool operator<(const Coordinate &pt) const {
       if (x == pt.x)
          return (y <= pt.y);
       return (x < pt.x);
    }
 };
 
-int side_of_line(const Point &point1, const Point &point2, const Point &point3);
-float distance_from_line(const Point &point1, const Point &point2, const Point &point3);
-bool point_in_triangle(const Point &p, const Point &point1, const Point &point2, const Point &point3);
-void find_hull(const std::vector<Point> &Sk, const Point P, const Point Q, std::vector<Point> &hull_points);
-void quick_hull(const std::vector<Point> &s, std::vector<Point> &hull_points);
+int side_of_line(const Coordinate &point1, const Coordinate &point2, const Coordinate &point3);
+float distance_from_line(const Coordinate &point1, const Coordinate &point2, const Coordinate &point3);
+bool point_in_triangle(const Coordinate &p, const Coordinate &point1, const Coordinate &point2, const Coordinate &point3);
+void find_hull(const std::vector<Coordinate> &Sk, const Coordinate P, const Coordinate Q, std::vector<Coordinate> &hull_points);
+void quick_hull(const std::vector<Coordinate> &s, std::vector<Coordinate> &hull_points);
 double clock_time(clock_t, clock_t);
 
 int main(int argc, char* argv[]) {
@@ -55,16 +55,16 @@ int main(int argc, char* argv[]) {
    if(do_single_run == 1) { 
       number_of_points = 10;
 
-      std::vector<Point> points;
+      std::vector<Coordinate> points;
       points.reserve(number_of_points);
-      std::vector<Point> hull_points;
+      std::vector<Coordinate> hull_points;
 
       // Random number generator
       std::mt19937 mersenne_twister(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
       // Generate random points
       for (int i = 0; i < number_of_points; ++i) {
-         Point pt;
+         Coordinate pt;
          pt.x = (float)(mersenne_twister() % 1000);
          pt.y = (float)(mersenne_twister() % 1000);
          points.push_back(pt);
@@ -110,16 +110,16 @@ int main(int argc, char* argv[]) {
    while(number_of_points <= FINAL_SIZE) {
       std::cout << "  " << number_of_points << std::endl;
 
-      std::vector<Point> points;
+      std::vector<Coordinate> points;
       points.reserve(number_of_points);
-      std::vector<Point> hull_points;
+      std::vector<Coordinate> hull_points;
 
       // Random number generator
       std::mt19937 mersenne_twister(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
       // Generate random points
       for (int i = 0; i < number_of_points; ++i) {
-         Point pt;
+         Coordinate pt;
          pt.x = (float)(mersenne_twister() % 1000);
          pt.y = (float)(mersenne_twister() % 1000);
          points.push_back(pt);
@@ -144,16 +144,16 @@ int main(int argc, char* argv[]) {
    return 0;
 }
 
-int side_of_line(const Point &point1, const Point &point2, const Point &point3) {
+int side_of_line(const Coordinate &point1, const Coordinate &point2, const Coordinate &point3) {
    return (point2.x - point1.x) * (point3.y - point1.y) - (point2.y - point1.y) * (point3.x - point1.x);
 }
 
-float distance_from_line(const Point &point1, const Point &point2, const Point &point3) {
+float distance_from_line(const Coordinate &point1, const Coordinate &point2, const Coordinate &point3) {
    return (std::abs((point2.y - point1.y) * point3.x - (point2.x - point1.x) * point3.y + point2.x * point1.y - point2.y * point1.x)
           / std::sqrt((point2.y - point1.y) * (point2.y - point1.y) + (point2.x - point1.x) * (point2.x - point1.x)));
 }
 
-bool point_in_triangle(const Point &p, const Point &point1, const Point &point2, const Point &point3) {
+bool point_in_triangle(const Coordinate &p, const Coordinate &point1, const Coordinate &point2, const Coordinate &point3) {
    float a = ((point2.y - point3.y) * (p.x - point3.x) + (point3.x - point2.x) * (p.y - point3.y)) / ((point2.y - point3.y) * (point1.x - point3.x) + (point3.x - point2.x) * (point1.y - point3.y));
    float b = ((point3.y - point1.y) * (p.x - point3.x) + (point1.x - point3.x) * (p.y - point3.y)) / ((point2.y - point3.y) * (point1.x - point3.x) + (point3.x - point2.x) * (point1.y - point3.y));
    float c = 1.0f - a - b;
@@ -161,19 +161,19 @@ bool point_in_triangle(const Point &p, const Point &point1, const Point &point2,
 }
 
 // http://www.cse.yorku.ca/~aaw/Hang/quick_hull/Algorithm.html
-void find_hull(const std::vector<Point> &Sk, const Point P, const Point Q, std::vector<Point> &hull_points) {
+void find_hull(const std::vector<Coordinate> &Sk, const Coordinate P, const Coordinate Q, std::vector<Coordinate> &hull_points) {
    if (Sk.size() == 0) return;
 
-   std::vector<Point> S0;
-   std::vector<Point> S1;
-   std::vector<Point> S2;
+   std::vector<Coordinate> S0;
+   std::vector<Coordinate> S1;
+   std::vector<Coordinate> S2;
 
-   float furthestDistance = 0.0f;
-   Point C;
+   float greatest_distance = 0.0f;
+   Coordinate C;
    for (const auto &pt : Sk) {
       float distance = distance_from_line(P, Q, pt);
-      if (distance > furthestDistance) {
-         furthestDistance = distance;
+      if (distance > greatest_distance) {
+         greatest_distance = distance;
          C = pt;
       }
    }
@@ -194,17 +194,17 @@ void find_hull(const std::vector<Point> &Sk, const Point P, const Point Q, std::
 }
 
 // http://www.cse.yorku.ca/~aaw/Hang/quick_hull/Algorithm.html
-void quick_hull(const std::vector<Point> &s, std::vector<Point> &hull_points) {
-   Point A = s[0];
-   Point B = s[s.size() - 1];
+void quick_hull(const std::vector<Coordinate> &s, std::vector<Coordinate> &hull_points) {
+   Coordinate A = s[0];
+   Coordinate B = s[s.size() - 1];
    hull_points.push_back(A);
    hull_points.push_back(B);
 
-   std::vector<Point> S1;
-   std::vector<Point> S2;
+   std::vector<Coordinate> S1;
+   std::vector<Coordinate> S2;
 
    for (auto it = s.begin() + 1; it != s.end() - 1; ++it) {
-      const Point pt = *it;
+      const Coordinate pt = *it;
       const int s1 = side_of_line(A, B, pt);
       const int s2 = side_of_line(B, A, pt);
       if (0 < s1) 
